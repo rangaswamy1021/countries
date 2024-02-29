94200210cd8f03326a54fa0b38aedb4be3718bd0
Ranga Swamy <33976154+rangaswamy1021@users.noreply.github.com>
2024-02-29 11:13:13 +0530

    Header and footer core components created

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {}
